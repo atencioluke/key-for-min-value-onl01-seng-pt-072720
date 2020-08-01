5ae611167fb209ba9f2cc32d9553d5fd11c9d3e0
Luke Atencio <atencioluke@gmail.com>
2020-08-01 06:33:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,8 @@ def key_for_min_value(name_hash)
   min_value = nil
   name_hash.each do |key, value|
     if min_value == nil || value < min_value
-      min_value = value
       min_key = key
-    else
-      return nil
+      min_value = value
     end
     min_key
   end
